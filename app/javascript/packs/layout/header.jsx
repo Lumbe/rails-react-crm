b1fa6a5dc248e3aee7aab0c788741d6b718e6783
Lumbe <just.me.sober@gmail.com>
2017-06-19 17:59:24 +0300

    yarn add react-router-dom & Header component

--- app/javascript/packs/layout/header.jsx
+++ app/javascript/packs/layout/header.jsx
@@ -7,11 +7,36 @@ class Header extends React.Component {
   }
 
   defaultProps() {
-    return {app_name: 'CRM'}
+    return {app_name: ''};
+  }
+
+  componentDidMount() {
+    // executes when component is mounted
+    this.setState({app_name: 'CRM'});
+  }
+
+  componentWillUnmount() {
+    // executes when component is unmounted
+  }
+
+  changeAppName(e) {
+    e.preventDefault();
+    var newAppName = window.prompt("Введите новое название приложения", this.state.app_name);
+    console.log(newAppName);
+    this.setState({app_name: newAppName});
   }
 
   render() {
-  return <div className="header">Header for {this.state.app_name}!</div>
+    return (
+      <div>
+        <div className="header">
+          Header for {this.state.app_name}!
+        </div>
+        <button onClick={this.changeAppName.bind(this)}>
+          Change app name
+        </button>
+      </div>
+    );
   }
 }
 
