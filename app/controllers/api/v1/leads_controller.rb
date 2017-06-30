a661d593aa2f25a9dfea85ddeb669edf444c4005
Lumbe <just.me.sober@gmail.com>
2017-06-30 03:34:28 +0300

    leads controller

--- app/controllers/api/v1/leads_controller.rb
+++ app/controllers/api/v1/leads_controller.rb
@@ -1,2 +1,7 @@
-class LeadsController < ApplicationController
+class Api::V1::LeadsController < ApplicationController
+  respond_to :json
+  def index
+    @leads = Lead.all
+    respond_with @leads
+  end
 end
