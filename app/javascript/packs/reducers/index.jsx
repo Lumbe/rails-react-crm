bef614ddc598b6ce908a146ca55c8ea15674c82d
Lumbe <just.me.sober@gmail.com>
2017-08-16 13:38:42 +0300

    reset redux store(and localStorage) on user logout

--- app/javascript/packs/reducers/index.jsx
+++ app/javascript/packs/reducers/index.jsx
@@ -1,10 +1,11 @@
 
 import {combineReducers} from 'redux';
 import {leads, lead} from './leadReducer';
-import {currentUser} from './userReducer';
+import currentUser from './authUserReducer';
 import {houseProjects, houseProject} from './houseProjectReducer';
 
-const rootReducer = combineReducers({
+
+const appReducer = combineReducers({
   // short hand property names
   leads,
   lead,
@@ -12,5 +13,13 @@ const rootReducer = combineReducers({
   houseProject,
   currentUser
 })
+
+const rootReducer = (state, action) => {
+  if (action.type === 'DESTROY_USER_SESSION_SUCCESS') {
+    state = undefined
+  }
+
+  return appReducer(state, action)
+}
 
 export default rootReducer;
