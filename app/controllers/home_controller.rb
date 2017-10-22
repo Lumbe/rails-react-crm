c0bb32d20bbcf317ca60a9aab8f4d794a6c3f1dd
Lumbe <just.me.sober@gmail.com>
2017-10-22 20:56:35 +0300

    home#index respond_to

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+
+  def index
+    respond_to do |format|
+      format.html
+      format.js
+      format.json
+    end
+  end
   def robots
     respond_to :text
   end
