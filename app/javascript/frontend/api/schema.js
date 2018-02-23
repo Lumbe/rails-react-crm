31ce2f1643088c22c95e48754391e36f1ecf97d3
Lumbe <just.me.sober@gmail.com>
2018-02-23 10:58:12 +0200

    cleanup

--- app/javascript/frontend/api/schema.js
+++ app/javascript/frontend/api/schema.js
@@ -1,16 +1,9 @@
-import { normalize, denormalize, schema } from 'normalizr';
+import { schema } from 'normalizr';
 
-// for tests
-window.normalize = normalize;
-window.denormalize = denormalize;
-
-// Define a users schema
 const userSchema = new schema.Entity('users');
 
-// Define your comments schema
 const departmentSchema = new schema.Entity('departments');
 
-// Define your article
 const lead = new schema.Entity('leads', {
   user: userSchema,
   assigned_to: userSchema,
@@ -19,5 +12,3 @@ const lead = new schema.Entity('leads', {
 
 export const leadListSchema = [lead];
 export const leadSchema = lead;
-
-// const normalizedData = normalize(originalData, lead);
